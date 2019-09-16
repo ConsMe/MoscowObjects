7417a1b54a74ffe7237fd38f827b405993e07eab
Alex M <alexandr.makarov@balance-pl.ru>
2019-09-16 20:15:32 +0300

    init commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('currencies:refresh')->everyMinute();
+        $schedule->command('currencies:refresh')->dailyAt('00:00');
     }
 
     /**
